5186936aa34f75a8a664ff667c14ad6acf3b0523
Anton Gunnarsson <gson.anton@hotmail.com>
2020-01-21 22:20:53 +0100

    Refactor card colors

--- src/App.js
+++ src/App.js
@@ -27,12 +27,7 @@ function App() {
     }
     setLoading(true)
     getActivities(settings.count).then(activities => {
-      setItems(
-        activities.map(item => ({
-          ...item,
-          color: colors[Math.floor(Math.random() * colors.length)]
-        }))
-      )
+      setItems(activities)
       setLoading(false)
     })
   }
@@ -65,23 +60,3 @@ const FormWrapper = styled(animated.div)`
   flex-direction: column;
   margin-bottom: 25px;
 `
-
-const colors = [
-  'hsl(0, 57%, 86%)',
-  'hsl(0, 72%, 70%)',
-  'hsl(23, 37%, 60%)',
-  'hsl(39, 57%, 70%)',
-  'hsl(58, 55%, 88%)',
-  'hsl(60, 62%, 75%)',
-  'hsl(97, 100%, 90%)',
-  'hsl(114, 66%, 78%)',
-  'hsl(129, 49%, 81%)',
-  'hsl(152, 41%, 82%)',
-  'hsl(172, 50%, 69%)',
-  'hsl(184, 33%, 65%)',
-  'hsl(199, 53%, 77%)',
-  'hsl(201, 49%, 80%)',
-  'hsl(288, 94%, 91%)',
-  'hsl(354, 88%, 81%)',
-  'hsl(357, 40%, 76%)'
-]
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useTransition, animated } from 'react-spring'
 import Card from './Card'
 
 export default function Cards({ items }) {
-  const transitions = useTransition(items, item => item.id, {
+  const itemsWithColors = useMemo(() => addColors(items), [items])
+
+  const transitions = useTransition(itemsWithColors, item => item.id, {
     from: {
       opacity: 0,
       transform: `perspective(600px) rotateX(-90deg)`
@@ -43,3 +45,36 @@ export default function Cards({ items }) {
 }
 
 const AnimatedCard = animated(Card)
+
+const addColors = items => {
+  const randomColorStartIdx = Math.floor(Math.random() * colors.length)
+
+  return items.map((item, idx) => {
+    let colorIdx = randomColorStartIdx + idx
+    if (colorIdx > colors.length - 1) colorIdx = colorIdx - colors.length
+    return {
+      ...item,
+      color: colors[colorIdx]
+    }
+  })
+}
+
+const colors = [
+  'hsl(58, 55%, 88%)',
+  'hsl(23, 37%, 60%)',
+  'hsl(97, 100%, 90%)',
+  'hsl(39, 57%, 70%)',
+  'hsl(114, 66%, 78%)',
+  'hsl(60, 62%, 75%)',
+  'hsl(354, 88%, 81%)',
+  'hsl(152, 41%, 82%)',
+  'hsl(184, 33%, 65%)',
+  'hsl(129, 49%, 81%)',
+  'hsl(0, 72%, 70%)',
+  'hsl(199, 53%, 77%)',
+  'hsl(357, 40%, 76%)',
+  'hsl(172, 50%, 69%)',
+  'hsl(288, 94%, 91%)',
+  'hsl(0, 57%, 86%)',
+  'hsl(201, 49%, 80%)'
+]
